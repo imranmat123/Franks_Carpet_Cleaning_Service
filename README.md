3bedab730d432183dabdbe2dbce5bcd2efe10205
imranmat123 <112811516+imranmat123@users.noreply.github.com>
2023-03-28 11:52:05 +0100

    The code for Frank's Carpet Cleaning Service has been modified to include an option for cleaning large rooms, in addition to small rooms. The program prompts the user to input the number of small and large rooms they would like cleaned, and then displays an estimate for the service. The prices for small and large rooms are different, and the program calculates the cost and tax separately for each room type. Finally, the total estimate is displayed, which includes the cost and tax for both small and large rooms. The estimate is valid for 30 days.

---
+++ README.md
@@ -0,0 +1,7 @@
+Exercise 6 on Udemy "Frank's Carpet Cleaning Service" requires a C++ program that calculates the cost and tax for cleaning a given number of rooms, and displays the results as an estimate to the user.
+
+The pseudocode is provided to prompt the user for the number of rooms, display the price per room, calculate the cost and tax, and display the estimate expiration time. The code uses const variables to store the price per room, tax rate, and validity period of the estimate.
+
+The code includes standard input/output statements and arithmetic operations to calculate the cost, tax, and total estimate based on the user input. The output is displayed in the console in the format of a detailed estimate with a breakdown of the cost, tax, and validity period.
+
+Overall, this exercise provides practice with input/output, arithmetic operations, and using constants in C++.
--- main.cpp
+++ main.cpp
@@ -12,22 +12,30 @@ using namespace std;
 int main() {
     cout << "Hey, welcome to franks carpet cleaning service!" << std::endl;
 
-    cout << "\nHow many rooms would you liked cleaned? ";
-    int numberOfRooms {0};
+    cout << "\nHow many small rooms would you liked cleaned? ";
+    int numberSmallOfRooms {0};
+    cin >> numberSmallOfRooms;
+    cout << "\nHow many large rooms would you liked cleaned? ";
+    int numberLargeOfRooms {0};
+    cin >> numberLargeOfRooms;
 
-    const double pricePerRoom{30.0};
+
+    const double pricePerSmallRoom{25.0};
+    const double pricePerLargeRoom{35.0};
     const double taxRate {0.06};
     const int validFor{30};  //days
 
-    cin >> numberOfRooms;
     cout << "\nEstimate for carpet cleaning service: \n";
-    cout << "Number of rooms: " << numberOfRooms << endl;
-    cout << "Price per room: £" << pricePerRoom << endl;
-    cout << "Cost: £" << pricePerRoom * numberOfRooms << endl;
-    cout << "Tax: £" << numberOfRooms * pricePerRoom * taxRate << endl;
+    cout << "Number of small rooms: " << numberSmallOfRooms << endl;
+    cout << "Number of small rooms: " << numberLargeOfRooms << endl;
+    cout << "Price per small room: £" << pricePerSmallRoom << endl;
+    cout << "Price per large room: £" << pricePerLargeRoom << endl;
+    cout << "Cost: £" << (numberSmallOfRooms * pricePerSmallRoom) + (numberLargeOfRooms * pricePerLargeRoom) << endl;
+    cout << "Tax: £" << (numberLargeOfRooms * pricePerLargeRoom * taxRate) + (numberSmallOfRooms * pricePerSmallRoom * taxRate) << endl;
     cout << "================================" << endl;
 
-    cout << "Total estimate: £" << (pricePerRoom * numberOfRooms) + (numberOfRooms * pricePerRoom * taxRate) << endl;
+    cout << "Total estimate: £" << (pricePerSmallRoom * numberSmallOfRooms) + (numberSmallOfRooms * pricePerSmallRoom * taxRate) +
+            (pricePerLargeRoom * numberLargeOfRooms) + (pricePerLargeRoom * numberLargeOfRooms * taxRate) << endl;
     cout << "This estimate is valid for: " << validFor << " days" <<endl;
 
     return 0;
